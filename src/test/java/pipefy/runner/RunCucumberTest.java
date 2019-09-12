37760b807e84cfe57c81dd28bcd5502771d7ff44
raulchoque <yfert_0209@hotmail.com>
2019-09-12 08:41:01 -0400

    add runner class to test all features of our project

--- src/test/java/pipefy/runner/RunCucumberTest.java
+++ src/test/java/pipefy/runner/RunCucumberTest.java
@@ -12,11 +12,29 @@
 
 package pipefy.runner;
 
+import core.report.GeneratorReport;
+import core.selenium.WebDriverManager;
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.AfterTest;
+
 /**
- * RunCucunberTest class.
+ * RunCucumberTest class, run all test of framework.
  *
  * @author Raul Choque
  * @version 0.0.1
  */
-public class RunCucunberTest {
+@CucumberOptions(features = "src/test/resources/features",
+        glue = {"pipefy/steps_implementation", "pipefy/hooks"},
+        plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
+public class RunCucumberTest extends AbstractTestNGCucumberTests {
+
+    /**
+     * Run the last test in our framework.
+     */
+    @AfterTest
+    public void runLastTest() {
+        WebDriverManager.getInstance().getDriver().close();
+        GeneratorReport.getInstance().generateReport();
+    }
 }
